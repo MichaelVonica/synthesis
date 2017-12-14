fa0b8726e81e51f2824f2ae518cff7db6bb763dc
shawn hice <shawnhice@gmail.com>
2017-12-14 12:29:51 -0800

    Now created actual new launcher after thinking about the impact of editing the location of the old one.

--- launcher/launcher/Properties/Settings.Designer.cs
+++ launcher/launcher/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SynthesisLauncher.Properties
+namespace launcher.Properties
 {
 
 
